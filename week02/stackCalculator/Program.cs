3010d571d67e85bf058bd7901edcb333c9fa6a10
Alexander <sacha05bugaev@gmail.com>
2024-02-27 23:50:12 +0300

    add solution

---
+++ week02/stackCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using Calc;
+using Tests;
+using System;
+
+namespace Solution
+{
+    static class Program
+    {
+        static void Main()
+        {
+            TestForCalculator.Test();
+
+            Console.WriteLine("Enter an expression in postfix form: ");
+            try
+            {
+                string? inputString = Console.ReadLine();
+                if (inputString is null)
+                {
+                    throw new ArgumentNullException();
+                }
+                Console.WriteLine("\nResult: {0}", Calculator.Compute(inputString));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nAn error occured");
+            }
+        }
+    }
+}
