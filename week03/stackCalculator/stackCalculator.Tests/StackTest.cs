9e9376477d19e144f7dca94b64c51869c39b8766
Alexander <sacha05bugaev@gmail.com>
2024-03-04 23:20:52 +0300

    add unit tests

---
+++ week03/stackCalculator/stackCalculator.Tests/StackTest.cs
@@ -0,0 +1,67 @@
+﻿using Stack;
+using System.Runtime.CompilerServices;
+
+namespace stackCalculator.Tests
+{
+    public class StackTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        public static IEnumerable<TestCaseData> StackTypeTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new StackOnPointers<float>());
+                yield return new TestCaseData(new StackOnArray<float>());
+            }
+        }
+
+        [TestCaseSource(nameof(StackTypeTestCases))]
+        public void TestForStack_PushOneElement_GetTheSameElement(IStack<float> stack)
+        {
+            stack.Push(5.56f);
+            Assert.That(stack.Pop(), Is.EqualTo(5.56f));
+        }
+
+        [TestCaseSource(nameof(StackTypeTestCases))]
+        public void TestForStack_PushTwoElements_GetElementsInReverseOrder(IStack<float> stack)
+        {
+            stack.Push(0);
+            stack.Push(22);
+            Assert.That(stack.Pop(), Is.EqualTo(22));
+            Assert.That(stack.Pop(), Is.EqualTo(0));
+        }
+
+        [TestCaseSource(nameof(StackTypeTestCases))]
+        public void TestForStack_PushPopAndPushAgain_GetElements(IStack<float> stack)
+        {
+            stack.Push(432);
+            stack.Pop();
+            stack.Push(-100);
+            Assert.That(stack.Pop(), Is.EqualTo(-100));
+        }
+
+        [TestCaseSource(nameof(StackTypeTestCases))]
+        public void TestForStack_PopOutOfAnEmptyStack_ThrowException(IStack<float> stack)
+        {
+            Assert.Throws<InvalidOperationException>(delegate { stack.Pop(); });
+        }
+
+        [TestCaseSource(nameof(StackTypeTestCases))]
+        public void TestForStack_MorePushes_GetElements(IStack<float> stack)
+        {
+            int numberOfPushes = 100;
+            for (int i = 0; i < numberOfPushes; ++i)
+            {
+                stack.Push(i);
+            }
+            for (int i = 0; i < numberOfPushes; ++i)
+            {
+                Assert.That(stack.Pop(), Is.EqualTo((float)(numberOfPushes - i - 1)));
+            }
+        }
+    }
+}
---
+++ week03/stackCalculator/stackCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using Calc;
+using System;
+
+namespace Solution
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("-----Stack calculator-----");
+            Console.WriteLine("\nEnter an expression in postfix form: ");
+            try
+            {
+                string? inputString = Console.ReadLine();
+                if (inputString is null)
+                {
+                    throw new ArgumentNullException();
+                }
+                Console.WriteLine("\nResult: {0}", Calculator.Compute(inputString));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nAn error occured");
+            }
+        }
+    }
+}
