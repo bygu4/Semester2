ec154a4ca152f4e9fb02e4d7b6f46e2f680d1178
Alexander <sacha05bugaev@gmail.com>
2024-03-13 19:15:51 +0300

    add support of variable length codes

--- week03/LZW/LZW/Encoder.cs
+++ week03/LZW/LZW/Encoder.cs
@@ -1,4 +1,5 @@
 ﻿namespace LZWEncoder;
+
 using Trie;
 using BurrowsWheeler;
 using System;
@@ -13,37 +14,63 @@ public static class Encoder
         return buffer;
     }
 
-    private static (string, int, int) Compress_Setup(Trie<int> dictionary,
+    private static int GetLengthInBits(int number)
+    {
+        return (int)Math.Floor(Math.Log2(number)) + 1;
+    }
+
+    private static int LeftBitShift(int number, int shift)
+    {
+        if (shift < 0)
+        {
+            return number >> -shift;
+        }
+        else
+        {
+            return number << shift;
+        }
+    }
+
+    private static int RightBitShift(int number, int shift)
+    {
+        return LeftBitShift(number, -shift);
+    }
+
+    private static void WriteCode(FileStream stream, 
+        int code, int lengthOfCode, ref int buffer, ref int shift)
+    {
+        int lengthOfByte = GetLengthInBits(byte.MaxValue);
+        while (lengthOfCode > 0)
+        {
+            int bitsToAdd = LeftBitShift(code, lengthOfByte - lengthOfCode - shift);
+            int sum = buffer | bitsToAdd;
+            int addedBits = RightBitShift(sum - buffer, lengthOfByte - lengthOfCode - shift);
+            buffer = sum;
+            shift += lengthOfCode;
+            lengthOfCode = shift - lengthOfByte;
+            if (lengthOfCode >= 0)
+            {
+                stream.WriteByte(Convert.ToByte(buffer));
+                buffer = 0;
+                shift = 0;
+            }
+            code -= addedBits;
+        }
+    }
+
+    private static (string, int) Compress_Setup(Trie<int> dictionary,
         FileStream inputStream, FileStream resultStream)
     {
         byte[] inputBytes = GetBytes(inputStream);
         string inputData = System.Text.Encoding.UTF8.GetString(inputBytes);
         (inputData, int BWTPosition) = BWT.Transform(inputData);
-        int numberOfSpecialCharacters = 0;
         foreach (char character in inputData)
         {
             if (dictionary.Add(character, dictionary.Size))
             {
-                resultStream.WriteByte(Convert.ToByte(character));
-                ++numberOfSpecialCharacters;
+                resultStream.Write(BitConverter.GetBytes(character));
             }
         }
-        return (inputData, BWTPosition, numberOfSpecialCharacters);
-    }
-
-    private static (string, int) Decompress_Setup(Dictionary<int, string> dictionary, 
-        FileStream inputStream, FileStream resultStream)
-    {
-        byte[] inputBytes = GetBytes(inputStream);
-        int numberOfSpecialCharacters = inputBytes[inputBytes.Length - 1];
-        int BWTPosition = inputBytes[inputBytes.Length - 2];
-        while (dictionary.Count < numberOfSpecialCharacters)
-        {
-            dictionary.Add(dictionary.Count, Convert.ToChar(inputBytes[dictionary.Count]).ToString());
-        } 
-        dictionary.TryAdd(0, "");
-        string inputData = System.Text.Encoding.UTF8.GetString(inputBytes).Substring(
-            numberOfSpecialCharacters, inputBytes.Length - numberOfSpecialCharacters - 1);
         return (inputData, BWTPosition);
     }
 
@@ -53,9 +80,12 @@ public static class Encoder
         FileStream resultStream = File.OpenWrite(filePath + ".zipped");
         long inputFileLength = inputStream.Length;
         Trie<int> words = new Trie<int>();
-        (string inputData, int BWTPosition, int numberOfSpecialCharacters) = 
-            Compress_Setup(words, inputStream, resultStream);
+        (string inputData, int BWTPosition) = Compress_Setup(words, inputStream, resultStream);
+        int numberOfSpecialCharacters = words.Size;
 
+        int lengthOfCode = GetLengthInBits(numberOfSpecialCharacters);
+        int buffer = 0;
+        int shift = 0;
         string currentWord = "";
         for (int i = 0; i < inputData.Length; ++i)
         {
@@ -67,17 +97,62 @@ public static class Encoder
             }
             else
             {
-                resultStream.WriteByte(Convert.ToByte(words.Value(currentWord)));
+                int code = words.Value(currentWord);
+                WriteCode(resultStream, code, lengthOfCode, ref buffer, ref shift);
                 words.Add(temp, words.Size);
+                lengthOfCode = int.Max(lengthOfCode, GetLengthInBits(words.Size));
                 currentWord = next;
             }
         }
-        resultStream.WriteByte(Convert.ToByte(words.Value(currentWord)));
-        resultStream.WriteByte(Convert.ToByte(BWTPosition));
-        resultStream.WriteByte(Convert.ToByte(numberOfSpecialCharacters));
+        WriteCode(resultStream, words.Value(currentWord), lengthOfCode, ref buffer, ref shift);
+        if (shift > 0)
+        {
+            resultStream.WriteByte(Convert.ToByte(buffer));
+        }
+        resultStream.WriteByte(Convert.ToByte(shift));
+        resultStream.Write(BitConverter.GetBytes(BWTPosition));
+        resultStream.Write(BitConverter.GetBytes(numberOfSpecialCharacters));
         long resultFileLength = resultStream.Length;
         resultStream.Close();
         return (float)inputFileLength / resultFileLength;
+    }
+
+    private static int ReadCode(byte[] inputBytes, int lengthOfCode, 
+        ref int currentIndex, ref int shift)
+    {
+        int lengthOfByte = GetLengthInBits(byte.MaxValue);
+        int code = 0;
+        while (lengthOfCode > 0)
+        {
+            int currentByte = inputBytes[currentIndex];
+            int bitsToAdd = (currentByte & (byte.MaxValue >> shift)) -
+                (currentByte & (byte.MaxValue >> (shift + lengthOfCode)));
+            int addedBits = RightBitShift(bitsToAdd, lengthOfByte - lengthOfCode - shift);
+            shift += lengthOfCode;
+            lengthOfCode = shift - lengthOfByte;
+            if (lengthOfCode >= 0)
+            {
+                ++currentIndex;
+                shift = 0;
+            }
+            code += addedBits;
+        }
+        return code;
+    }
+
+    private static (byte[], int, int) Decompress_Setup(Dictionary<int, string> dictionary,
+    FileStream inputStream, FileStream resultStream)
+    {
+        byte[] inputBytes = GetBytes(inputStream);
+        int numberOfSpecialCharacters = BitConverter.ToInt32(inputBytes, inputBytes.Length - 4);
+        for (int i = 0; i < numberOfSpecialCharacters; ++i)
+        {
+            dictionary.Add(dictionary.Count, BitConverter.ToChar(inputBytes, i * 2).ToString());
+        }
+        dictionary.TryAdd(0, "");
+        int BWTPosition = BitConverter.ToInt32(inputBytes, inputBytes.Length - 8);
+        int lastByteShift = inputBytes[inputBytes.Length - 9];
+        return (inputBytes, BWTPosition, lastByteShift);
     }
 
     public static void Decompress(string filePath)
@@ -86,28 +161,37 @@ public static class Encoder
         FileStream resultStream = File.OpenWrite(
             Path.Join(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath)));
         Dictionary<int, string> words = new Dictionary<int, string>();
-        (string inputData, int BWTPosition) = Decompress_Setup(words, inputStream, resultStream);
+        (byte[] inputBytes, int BWTPosition, int lastByteShift) = 
+            Decompress_Setup(words, inputStream, resultStream);
 
-        char currentCode = inputData[0];
+        int lengthOfCode = GetLengthInBits(words.Count);
+        int currentIndex = words.Count * 2;
+        int shift = 0;
+        int currentCode = ReadCode(inputBytes, lengthOfCode, ref currentIndex, ref shift);
         string encodedData = words[currentCode];
-        for (int i = 1; i < inputData.Length - 1; ++i)
+        while (currentIndex < inputBytes.Length - 9)
         {
-            char next = inputData[i];
+            lengthOfCode = int.Max(lengthOfCode, GetLengthInBits(words.Count + 1));
+            int next = ReadCode(inputBytes, lengthOfCode, ref currentIndex, ref shift);
             string output;
-            string addition;
+            string wordToAdd;
             if (words.ContainsKey(next))
             {
                 output = words[next];
-                addition = words[currentCode] + words[next][0];
+                wordToAdd = words[currentCode] + words[next][0];
             }
             else
             {
                 output = words[currentCode] + words[currentCode][0];
-                addition = output;
+                wordToAdd = output;
             }
             encodedData += output;
-            words.Add(words.Count, addition);
+            words.Add(words.Count, wordToAdd);
             currentCode = next;
+            if (currentIndex == inputBytes.Length - 10 && shift >= lastByteShift)
+            {
+                break;
+            }
         }
         string result = BWT.ReverseTransform(encodedData, BWTPosition);
         resultStream.Write(System.Text.Encoding.UTF8.GetBytes(result));
--- week03/LZW/LZW/Program.cs
+++ week03/LZW/LZW/Program.cs
@@ -1,21 +1,32 @@
 ﻿using LZWEncoder;
 using System;
 
-if (args.Length != 2)
+if (args.Length != 2 || args[1] != "-c" && args[1] != "-u")
 {
-    Console.WriteLine("Incorrect format. Try: LZW {path of the file} {-c | -u}\n" +
+    Console.WriteLine("Incorrect format. Try: {path of the file} {-c | -u}\n" +
         "c - compress file\n" +
         "u - decompress file");
     return;
 }
-if (args[1] == "-c")
+try
 {
-    float ratio = Encoder.Compress(args[0]);
-    Console.WriteLine("File was compressed");
-    Console.WriteLine("Compression ratio: " + ratio);
+    if (args[1] == "-c")
+    {
+        float ratio = Encoder.Compress(args[0]);
+        Console.WriteLine("File was compressed");
+        Console.WriteLine("Compression ratio: " + ratio);
+    }
+    else if (args[1] == "-u")
+    {
+        Encoder.Decompress(args[0]);
+        Console.WriteLine("File was decompressed");
+    }
 }
-if (args[1] == "-u")
+catch (Exception e) when (e is FileNotFoundException || e is DirectoryNotFoundException)
 {
-    Encoder.Decompress(args[0]);
-    Console.WriteLine("File was decompressed");
+    Console.WriteLine("Error: file not found");
+}
+catch
+{
+    Console.WriteLine("An error occured");
 }
