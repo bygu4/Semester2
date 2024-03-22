4dfc3eb7111e2ed91ee75003b84ad6ff05934331
bygu4 <sacha05bugaev@gmail.com>
2024-03-22 18:51:12 +0300

    test ci

--- scripts/windowsBuild.bat
+++ scripts/windowsBuild.bat
@@ -4,9 +4,9 @@ cd ..
 
 for /r %%i in (*.sln) do (
     dotnet build %%i --nologo -clp:NoSummary -v:q
-    if errorlevel 0 (
+    if %errorlevel% equ 0 (
         dotnet test %%i --no-build --nologo -v:q
-        if errorlevel 0 (
+        if %errorlevel% equ 0 (
             echo - %%~nxi: test passed
         ) else (
             echo - %%~nxi: test failed
