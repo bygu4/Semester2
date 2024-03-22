8812f40d2af9c313353938f547596477cbcec5b4
bygu4 <sacha05bugaev@gmail.com>
2024-03-22 18:16:04 +0300

    test ci

--- scripts/windowsBuild.bat
+++ scripts/windowsBuild.bat
@@ -1,6 +1,6 @@
 @echo off
 
-for /r %%i in (..\*.sln) do (
+for /r %%i in (*.sln) do (
     dotnet build %%i --nologo -clp:NoSummary -v:q
     if errorlevel 0 (
         dotnet test %%i --no-build --nologo -clp:NoSummary -v:q
