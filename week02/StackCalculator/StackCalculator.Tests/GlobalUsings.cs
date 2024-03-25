3f30be2debbabd53baf37fddcf98449b95ed4c92
bygu4 <sacha05bugaev@gmail.com>
2024-03-25 12:05:17 +0300

    change headers

--- week02/StackCalculator/StackCalculator.Tests/GlobalUsings.cs
+++ week02/StackCalculator/StackCalculator.Tests/GlobalUsings.cs
@@ -1,1 +1,6 @@
+// <copyright file="GlobalUsings.cs" company="SPBU">
+// Copyright (c) Alexander Bugaev 2024. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
 global using NUnit.Framework;
