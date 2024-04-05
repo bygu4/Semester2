e88a3208e2be582d7041fe27349a7bd70101e213
bygu4 <sacha05bugaev@gmail.com>
2024-04-05 19:11:56 +0300

    make project a library

--- week04/UniqueList/UniqueList/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// <copyright file="Program.cs" company="SPBU">
-// Copyright (c) Alexander Bugaev 2024. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-// </copyright>
-
-Console.WriteLine(string.Empty);
