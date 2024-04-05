9c55ef9b777e05f7e5e4bf5e6f2ba66de497a5f5
bygu4 <sacha05bugaev@gmail.com>
2024-04-05 19:21:45 +0300

    minor changes

--- week05/Routers/Routers.Tests/NetworkTest.cs
+++ week05/Routers/Routers.Tests/NetworkTest.cs
@@ -51,19 +51,19 @@ public class NetworkTest
         {
             yield return new TestCaseData("EmptyFile.txt", "").Returns(true);
             yield return new TestCaseData("SingleRouter.txt", "").Returns(true);
-            yield return new TestCaseData("TwoConnectedRouters.txt", "0 1 2").Returns(true);
+            yield return new TestCaseData("TwoConnectedRouters.txt", "0, 1, 2").Returns(true);
             yield return new TestCaseData("TwoDisconnectedRouters.txt", "").Returns(false);
             yield return new TestCaseData(
-                "ThreeRoutersConnectedWithoutCycle.txt", "12 3 1; 5 1 2").Returns(true);
+                "ThreeRoutersConnectedWithoutCycle.txt", "12, 3, 1; 5, 1, 2").Returns(true);
             yield return new TestCaseData(
-                "ThreeRoutersConnectedWithCycle.txt", "10 1 2; 6 3 2").Returns(true);
-            yield return new TestCaseData("TwoRoutersConnectedTwice.txt", "55 1 2").Returns(true);
-            yield return new TestCaseData("NetworkWithLoop.txt", "13 1 2").Returns(true);
+                "ThreeRoutersConnectedWithCycle.txt", "10, 1, 2; 6, 3, 2").Returns(true);
+            yield return new TestCaseData("TwoRoutersConnectedTwice.txt", "55, 1, 2").Returns(true);
+            yield return new TestCaseData("NetworkWithLoop.txt", "13, 1, 2").Returns(true);
             yield return new TestCaseData(
-                "DisconnectedNetworkOfFiveRouters.txt", "100 1 5; 34 2 4; 12 1 3").Returns(false);
+                "DisconnectedNetworkOfFiveRouters.txt", "100, 1, 5; 34, 2, 4; 12, 1, 3").Returns(false);
             yield return new TestCaseData(
                 "FiveRoutersConnectedWithCycles.txt",
-                "100 2 5; 70 3 5; 42 1 3; 15 4 5").Returns(true);
+                "100, 2, 5; 70, 3, 5; 42, 1, 3; 15, 4, 5").Returns(true);
         }
     }
 
@@ -73,7 +73,7 @@ public class NetworkTest
         (int, (int, int))[] connections = new (int, (int, int))[elements.Length];
         for (int i = 0; i < elements.Length; ++i)
         {
-            string[] split = elements[i].Split(' ');
+            string[] split = elements[i].Split(", ");
             int capacity = int.Parse(split[0]);
             (int, int) routers = (int.Parse(split[1]), int.Parse(split[2]));
             connections[i] = (capacity, routers);
