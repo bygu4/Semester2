279ad420afa32d764f80fce782abd4750d2dd1f2
Alexander <sacha05bugaev@gmail.com>
2024-04-18 13:22:15 +0300

    change configuration

--- scripts/windowsBuild.bat
+++ scripts/windowsBuild.bat
@@ -3,12 +3,12 @@
 cd ..
 
 for /r %%i in (*.sln) do (
-    dotnet build %%i --nologo -clp:NoSummary -v:q
+    dotnet build %%i --nologo -clp:NoSummary -v:m
     if errorlevel 1 (
         echo - %%~nxi: build failed
         exit 1
     ) else (
-        dotnet test %%i --no-build --nologo -v:q
+        dotnet test %%i --no-build --nologo -v:m
         if errorlevel 1 (
             echo - %%~nxi: test failed
             exit 1
