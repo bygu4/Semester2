28c3080affa9379ee56d9310bbdebe75904aa691
Alexander <sacha05bugaev@gmail.com>
2024-04-19 00:32:45 +0300

    update linuxBuild script

--- scripts/linuxBuild.bash
+++ scripts/linuxBuild.bash
@@ -4,11 +4,10 @@ for f in $(find .. -name "*.sln"); do (
         dotnet test $f --no-build --nologo -v:m
         if [ $? -eq 0 ]; then
             echo - ${f##*/}: test passed
-        elif [ $? -eq 4 ]; then
+        elif [ $? -eq 3 ]; then
             echo - ${f##*/}: test aborted
         else
             echo - ${f##*/}: test failed
-            exit 1
         fi
     else
         echo - ${f##*/}: build failed
