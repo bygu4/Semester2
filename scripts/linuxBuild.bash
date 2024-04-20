cda9ea9b1f4f67dc86cd65b2d862891567691ed2
Alexander <sacha05bugaev@gmail.com>
2024-04-20 22:22:11 +0300

    errorcode checkout

--- scripts/linuxBuild.bash
+++ scripts/linuxBuild.bash
@@ -2,13 +2,12 @@ for f in $(find .. -name "*.sln"); do (
     dotnet build $f --nologo -clp:NoSummary  -v:m
     if [ $? -eq 0 ]; then
         dotnet test $f --no-build --nologo -v:m
+        echo $?
         if [ $? -eq 0 ]; then
             echo - ${f##*/}: test passed
-        elif [ $? -eq 3 ]; then
-            echo - ${f##*/}: test aborted
         else
             echo - ${f##*/}: test failed
-            exit 1
+            exit $?
         fi
     else
         echo - ${f##*/}: build failed
