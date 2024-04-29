9f300c04ed1588e786e9bdb1d5d1ff7dda7bccbd
bygu4 <sacha05bugaev@gmail.com>
2024-04-29 23:15:20 +0300

    add stylecop

--- week08/SkipList/SkipList.cs
+++ week08/SkipList/SkipList.cs
@@ -1,7 +1,17 @@
-﻿namespace SkipList;
+﻿// <copyright file="SkipList.cs" company="SPBU">
+// Copyright (c) Alexander Bugaev 2024. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace SkipList;
 
 using System.Collections;
 
+/// <summary>
+/// Implementation of Skip List data structure.
+/// Elements in this collection are contained in the ascending order.
+/// </summary>
+/// <typeparam name="T">Type of contained elements.</typeparam>
 public class SkipList<T> : IList<T>
     where T : IComparable<T>
 {
@@ -11,6 +21,9 @@ public class SkipList<T> : IList<T>
     private Random random;
     private bool invalidateEnumerator;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SkipList{T}"/> class.
+    /// </summary>
     public SkipList()
     {
         this.topLevelHead = new Element(default, null, null);
@@ -18,11 +31,23 @@ public class SkipList<T> : IList<T>
         this.random = new Random(DateTime.Now.Millisecond);
     }
 
+    /// <summary>
+    /// Gets number of elements contained in the collection.
+    /// </summary>
     public int Count { get; private set; }
 
+    /// <summary>
+    /// Gets a value indicating whether the collection is read-only.
+    /// </summary>
     public bool IsReadOnly { get => false; }
 
-
+    /// <summary>
+    /// Gets or sets element on specific position in the collection.
+    /// </summary>
+    /// <param name="index">Zero-based index of the element in the collection.</param>
+    /// <returns>Element on given position in the collection.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Index is out of collection bounds.</exception>
+    /// <exception cref="NotSupportedException">Collection doesn't support setting element by index.</exception>
     public T this[int index]
     {
         get
@@ -33,25 +58,29 @@ public class SkipList<T> : IList<T>
             }
 
             int i = 0;
-            foreach (var value in this)
+            foreach (var item in this)
             {
                 if (i == index)
                 {
-                    return value;
+                    return item;
                 }
 
                 ++i;
             }
 
-            throw new NullReferenceException();
+            throw new ArgumentOutOfRangeException(nameof(index));
         }
 
         set => throw new NotSupportedException();
     }
 
-    public void Add(T value)
+    /// <summary>
+    /// Add element to the collection.
+    /// </summary>
+    /// <param name="item">Element to add.</param>
+    public void Add(T item)
     {
-        var newLevelElement = this.AddRecursion(this.topLevelHead, value);
+        var newLevelElement = this.AddRecursion(this.topLevelHead, item);
         if (newLevelElement != null)
         {
             this.CreateNewLevel(newLevelElement);
@@ -61,11 +90,22 @@ public class SkipList<T> : IList<T>
         this.invalidateEnumerator = true;
     }
 
-    public void Insert(int index, T value) => throw new NotSupportedException();
+    /// <summary>
+    /// Add element to specific position of the collection.
+    /// </summary>
+    /// <param name="index">Zero-based index to insert element to.</param>
+    /// <param name="item">Element to insert.</param>
+    /// <exception cref="NotSupportedException">Operation is not supported.</exception>
+    public void Insert(int index, T item) => throw new NotSupportedException();
 
-    public bool Remove(T value)
+    /// <summary>
+    /// Remove given element from the collection.
+    /// </summary>
+    /// <param name="item">Element to remove.</param>
+    /// <returns>A value indicating whether the element was removed.</returns>
+    public bool Remove(T item)
     {
-        var elementRemoved = this.RemoveRecursion(this.topLevelHead, value);
+        var elementRemoved = this.RemoveRecursion(this.topLevelHead, item);
         this.RemoveEmptyLevels();
 
         if (elementRemoved)
@@ -77,8 +117,15 @@ public class SkipList<T> : IList<T>
         return elementRemoved;
     }
 
+    /// <summary>
+    /// Remove element from specific position of the collection.
+    /// </summary>
+    /// <param name="index">Zero-based index to remove element from.</param>
     public void RemoveAt(int index) => this.Remove(this[index]);
 
+    /// <summary>
+    /// Remove all elements from the collection.
+    /// </summary>
     public void Clear()
     {
         while (this.Count > 0)
@@ -87,14 +134,25 @@ public class SkipList<T> : IList<T>
         }
     }
 
-    public bool Contains(T value) => this.FindElement(this.topLevelHead, value) != null;
+    /// <summary>
+    /// Check if given element is present in the collection.
+    /// </summary>
+    /// <param name="item">Element to find.</param>
+    /// <returns>A value indicating whether the element was found in the collection.</returns>
+    public bool Contains(T item) => this.FindElement(this.topLevelHead, item) != null;
 
-    public int IndexOf(T value)
+    /// <summary>
+    /// Get position of first occurrence of given element in the collection.
+    /// </summary>
+    /// <param name="item">Element to find.</param>
+    /// <returns>Zero-based index of first occurrence of the element in the collection.
+    /// If element was not found, -1 is returned.</returns>
+    public int IndexOf(T item)
     {
         int i = 0;
-        foreach (var currentValue in this)
+        foreach (var currentItem in this)
         {
-            var comparison = currentValue.CompareTo(value);
+            var comparison = currentItem.CompareTo(item);
             if (comparison == 0)
             {
                 return i;
@@ -110,6 +168,14 @@ public class SkipList<T> : IList<T>
         return -1;
     }
 
+    /// <summary>
+    /// Copy elements of the collection to given array starting from specific position.
+    /// </summary>
+    /// <param name="array">Array to copy elements to.</param>
+    /// <param name="startIndex">Zero-based index of array to start copying from.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Given index was negative.</exception>
+    /// <exception cref="ArgumentException">Destination array doesn't have enough space
+    /// to copy to starting from given index to the end of the array.</exception>
     public void CopyTo(T[] array, int startIndex)
     {
         ArgumentOutOfRangeException.ThrowIfNegative(startIndex);
@@ -120,20 +186,28 @@ public class SkipList<T> : IList<T>
                 $"Destination array doesn't have enough space starting from {nameof(startIndex)}");
         }
 
-        foreach (var value in this)
+        foreach (var item in this)
         {
-            array[startIndex++] = value;
+            array[startIndex++] = item;
         }
     }
 
+    /// <summary>
+    /// Get an instance of the collection Enumerator.
+    /// </summary>
+    /// <returns>An instance of class implementing IEnumerable.</returns>
     public IEnumerator<T> GetEnumerator() => new Enumerator(this);
-    
+
+    /// <summary>
+    /// Get an instance of the collection Enumerator.
+    /// </summary>
+    /// <returns>An instance of class implementing IEnumerable.</returns>
     IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
 
     private bool CoinFlip() => this.random.Next() % 2 == 0;
 
     private void CreateNewLevel(Element elementToAdd)
-        => this.topLevelHead = new(default, elementToAdd, this.topLevelHead);
+        => this.topLevelHead = new (default, elementToAdd, this.topLevelHead);
 
     private void RemoveEmptyLevels()
     {
@@ -142,7 +216,7 @@ public class SkipList<T> : IList<T>
             this.topLevelHead = this.topLevelHead.Down;
         }
     }
-    
+
     private Element? AddRecursion(Element current, T value)
     {
         while (current.Next != null && current.Next.Value != null &&
@@ -153,7 +227,7 @@ public class SkipList<T> : IList<T>
 
         var downElement = (current.Down != null) ?
             this.AddRecursion(current.Down, value) : null;
-        
+
         if (downElement != null || current.Down == null)
         {
             current.Next = new Element(value, current.Next, downElement);
